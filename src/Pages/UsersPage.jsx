92de502e2ed46b2a7588c8eaf4cbd2415f36fa93
jonraka <projectxpath@gmail.com>
2021-12-18 19:04:35 +0200

    userspage

--- src/Pages/UsersPage.jsx
+++ src/Pages/UsersPage.jsx
@@ -1,3 +1,33 @@
+import { useEffect, useState } from "react"
+import config from '../config.json';
+
 export default function UsersPage() {
-    return <div>Test</div>
+    const [data, setData] = useState({
+        loading: true,
+        data: null,
+        error: null
+    })
+
+    useEffect(() => {
+        fetch(config.api_endpoint + '/users').then(res => {
+            if (res.status === 404) throw new Error('API is offline, try again later');
+            return res.json();
+        }).then(res => {
+            if (res.error) throw new Error(res.error);
+
+            setData({
+                loading: false,
+                data: res.data,
+                error: null
+            })
+        }).catch(err => {
+            setData({
+                loading: false,
+                data: null,
+                error: err.message
+            })
+        })
+    }, []);
+
+    return <div><pre>{JSON.stringify(data, null, 4)}</pre></div>
 };
